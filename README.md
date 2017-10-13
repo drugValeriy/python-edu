57ba30217f7bdc9091323db9e1f50053a9851a68
Viktor Lukianenko <viktor.lukianenko@88technologies.com>
2017-10-13 11:13:59 +0300

    test

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # python-edu
 
 my baby stepts into python world
+
+```python
+import requests
+```
+
+
+copyright Markdown: TODO - read about this
--- random_god.py
+++ random_god.py
@@ -4,14 +4,37 @@ from random import randint
 random_number = randint(1, 10)
 
 guesses_left = 3
+own = 0
 # Start your game!
 while guesses_left > 0:
-  guess = int(raw_input("Guess my random from 1 to 10: "))
-  if guess == random_number:
-    print "You win!"
-    break
-  else:
-    print "Nope!"
-    guesses_left -= 1
-else:
-	print "You lose."
+    
+    while True:
+        guess = raw_input("Guess my random from 1 to 10: ")
+        int_guess = int(guess)
+        if isinstance(int_guess, int): 
+            break
+        else: 
+            print('DO YOU KNOW ENGLISH! TRIES! INTEGER BIATCH!')
+    if int(int_guess) == random_number:
+        print "You win!"
+        break
+    else:
+        print "Nope!"
+        guesses_left -= 1
+    if guesses_left == 0:
+        print "You suck"
+        while True:
+            new_try = raw_input('How many tries you need: ')
+            int_try = int(new_try)
+            if isinstance(int_try, int): 
+                break
+            else: 
+                print('DO YOU KNOW ENGLISH! TRIES! INTEGER BIATCH!')
+        if int_try == 0:
+            print "YOU OWN ME %d$ BIATCH. WHERE IS MY MONEY!? WHERE IS MY MONEY BIATCH?!" % own
+            break
+        own += int_try * 100
+        print "YOU OWN ME %d$ BIATCH" % own
+        guesses_left += int_try
+
+    
